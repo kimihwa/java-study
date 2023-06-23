b51b373dcfa688b0fc34bab7a1daa7cce48423f1
bit <bit@DESKTOP-EUNKPBS>
2023-06-23 15:48:35 +0900

    조건문

---
+++ chapter01/src/main/java/chapter01/Ex17Switch.java
@@ -0,0 +1,35 @@
+package chapter01;
+
+public class Ex17Switch {
+
+	public static void main(String[] args) {
+		char grade = ' ';
+		int score = 82;
+		
+		switch (score / 10) {
+		case 10 : 
+		case 9 : {
+			grade = 'A';
+			break;
+		}
+		case 8 : {
+			grade = 'B';
+			break;
+		}
+		case 7 : {
+			grade = 'C';
+			break;
+		}
+		case 6 : {
+			grade = 'D';
+			break;
+		}
+		default : {
+			grade = 'F';
+			break;
+		}
+		}
+		System.out.println("학점은 " + grade + "입니다." );
+	}
+
+}
