6b472075b8c94c4560fc0d47bbdeb3bbb5dbeac8
bit <bit@DESKTOP-EUNKPBS>
2023-06-23 16:46:13 +0900

    반복문1

---
+++ chapter01/src/main/java/chapter01/Ex21ForLoop.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Ex21ForLoop {
+
+	public static void main(String[] args) {
+		final int LOOP_COUNT = 10;
+	
+		for(int i = 0; i < LOOP_COUNT; i++) { 
+			System.out.println(i + ". hello");
+		}
+
+	}
+
+}
