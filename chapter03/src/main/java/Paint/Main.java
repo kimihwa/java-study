64cfd4fadfc3f4db0cd65ce97ddcb4d9efa2ef90
bit <bit@DESKTOP-EUNKPBS>
2023-06-28 11:49:19 +0900

    상속/다형성(오버라이드)

--- chapter03/src/main/java/Paint/Main.java
+++ chapter03/src/main/java/Paint/Main.java
@@ -6,15 +6,32 @@ public class Main {
 		Point point = new Point();
 		point.setX(100);
 		point.setY(50);
+
 		drawPoint(point);
+		point.show(false);
 		
 		Point point2 = new Point(200, 150);
 		drawPoint(point2);
+		point2.show(false);
+		
+		// 부모로 레퍼런싱
+//		Point point3 = new ColorPoint();
+//		point3.setX(200);
+//		point3.setY(100);
+		Point point3 = new ColorPoint(200, 100,"red");
+		// ColorPoint point4 = (ColorPoint)point3;
+		((ColorPoint)point3).setColor("red");
+		
+		drawPoint(point3);
 
 	}
 	
 	public static void drawPoint(Point point) {
 		point.show();
 	}
+	
+//	public static void drawColorPoint(ColorPoint colorPoint) {
+//		colorPoint.show();
+//	}
 
 }
