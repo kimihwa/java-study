18d085ea126ed2f3d6f90cd4e30530e33f31ed7e
bit <bit@DESKTOP-EUNKPBS>
2023-06-29 11:51:40 +0900

    Object 02

--- chapter04/src/main/java/chapter04/Point.java
+++ chapter04/src/main/java/chapter04/Point.java
@@ -1,5 +1,7 @@
 package chapter04;
 
+import java.util.Objects;
+
 public class Point {
 	private int x;
 	private int y;
@@ -13,4 +15,23 @@ public class Point {
 	public String toString() {
 		return "Point [x=" + x + ", y=" + y + "]";
 	}
+
+	//내용 기반 해시코드로 만듬
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		return x == other.x && y == other.y;
+	}
+	
 }
