8d8d71612d9ecae2e809e8bf14d4f634a5ba3b69
bit <bit@DESKTOP-EUNKPBS>
2023-06-29 15:37:36 +0900

    Date & Calender

--- chapter04/src/main/java/chapter04/CalendarTest.java
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -1,10 +1,12 @@
 package chapter04;
 
+import java.util.Calendar;
+
 public class CalendarTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Calendar cal = Calendar.getInstance();
+		
 	}
 
 }
